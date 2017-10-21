d52de02518eb6c1487168941bc3ab98b3265cf39
326623 <yangqp5@mail2.sysu.edu.cn>
2017-10-21 11:48:42 +0800

    done, need the file stream, and more tested and tweaking

--- implementation/racket-implementation/test.rkt
+++ implementation/racket-implementation/test.rkt
@@ -2,7 +2,7 @@
 
 (provide (all-defined-out))
 
-(define target (list "how"));
+(define target (list "how" "are"));
 (define corpus (list "how" "are" "you" "." "how" "well" "are" "you"));
 
 (define (count-string target corpus)
@@ -36,10 +36,44 @@
 ;; takes a sentence and output the probability computed using n-gram-model
 ;; sentence is a list of string
 ;; n as integer
-(define (n-gram-probability sentence n)
+;; don't accept n < 0
+(define (n-gram-probability sentence n corpus)
   ;; compute word by word
   ;; construct previous n-word context
 
-  ;; let previous-words be a stream that when evaluated provide the necessary previous words-context
-  (letrec ([previos-words ()]
-           [context (lambda )])))
+  ;; previous words contain the sequence of words reversed before the current word
+  (letrec ([previos-words null]
+           ;; need set!
+           [context (lambda (seq-bef previous-words g)
+                      ;; seq-bef        => sequence of words before current-word
+                      ;; previous-words => the words sequence to pick
+                      ;; g              => the remaining n-gram(remaining words needed to be added to seq-bef)
+                      (if (<= g 0)
+                          seq-bef
+                          (if (null? previous-words)
+                              (context (cons "<s>" seq-bef) null (- g 1))
+                              (context (cons (car previous-words) seq-bef) (cdr previous-words) (- g 1)))))]
+           [iter-by-word (lambda (un-processed previous-words)
+                           ;; returns a list of pair of (word . (n-1)-tuple) regarding previous words sequence
+                           (if (null? un-processed)
+                               null
+                               (let* ([context-seq (context null previous-words (- n 1))]
+                                      [next (iter-by-word (cdr un-processed) (cons (car un-processed) previous-words))]
+                                      [word-pair-context (cons context-seq (car un-processed))])
+                                 (cons word-pair-context next))))]
+           [count-in-corpus (lambda (produced-pairs)
+                              (if (null? produced-pairs)
+                                  null
+                                  (let* ([cur (car produced-pairs)]
+                                         [context-plus-word (append (car cur) (list (cdr cur)))]
+                                         [context-without-word (car cur)]
+                                         [dividend (count-string context-plus-word corpus)]
+                                         [divider (count-string context-without-word corpus)])
+                                    (cons (if (= divider 0)
+                                              0
+                                              (/ dividend divider)) (count-in-corpus (cdr produced-pairs))))))]
+           [mul-all (lambda (xs)
+                      (if (null? xs)
+                          1
+                          (* (car xs) (mul-all (cdr xs)))))])
+    (mul-all (count-in-corpus (iter-by-word sentence null)))))
