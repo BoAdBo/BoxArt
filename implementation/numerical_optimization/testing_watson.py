fc9f6cae549e2a233d1a109bdffcc6767689db0d
326623 <yangqp5@mail2.sysu.edu.cn>
2017-11-15 14:12:19 +0800

    finialize, let the server do its job

--- implementation/numerical_optimization/testing_watson.py
+++ implementation/numerical_optimization/testing_watson.py
@@ -16,6 +16,7 @@ temp_f = fun.sum_sq_watson
 temp_g = grad(temp_f)
 temp_G = jacobian(temp_g)
 status_out = 'status_report'
+MAX_TIME = 1
 
 def f(x):
     return temp_f(x)
@@ -26,75 +27,105 @@ def g(x):
 def G(x):
     return temp_G(x)
 
+class OverTime(Exception):
+    pass
+
+def put_prof(profile, output_file):
+    # this is where the information of the profile goes
+    sys.stdout = open(output_file, 'w')
+
+    # all methods return self, so you can string together commands, just like in ruby, very interesting
+    pstats.Stats(profile).sort_stats('time').print_stats('testing_watson')# print_stats take argument and return function in testing_watson.py
+
+    return 0
+
+
 def handler(signum, frame):
-    print ('Signal handler called with signal', signum)
-    
+    print('Sinal handler, already used time ' + str(MAX_TIME) + ' seconds; Signal number : ', signum, file = sys.__stdout__)
+    raise OverTime
 
 # function_name, output_file, info in string
-def status_report(function_name, output_file, info):
-    sys.stdout = sys.__stdout__
-    print(output_file)
-    sys.stdout = open(output_file, 'a')
-    print(output_file, ":", info)
+# info should contain information of the interrupted optimization methods, how long
+def status_report(output_file, info):
+    print(output_file, " for ", info, file = open(output_file, 'a'))
 
 def test_newtons(function, function_name, start = 2, to = 31, epsilon = 10 ** (-4)):
 
     for i in range(start, to):
-        x = np.zeros(i)
-
-        current_out_proc = str(i) + function_name + '.proc'
-
-        # this is where the information of iteration goes, the process
-        sys.stdout = open(current_out_proc, 'w')
-
+        # declare here to allow except to access
         pr = cProfile.Profile()
-        pr.enable()
-
-        e = function(f, g, G, x, epsilon)
-        print("the end of the result: ", f(e))
-
-        pr.disable()
-
+        current_out_proc = str(i) + function_name + '.proc'
         current_out_prof = str(i) + function_name + '.prof'
 
-        # this is where the information of the profile goes
-        sys.stdout = open(current_out_prof, 'w')
+        try:
+            x = np.zeros(i)
 
-        # all methods return self, so you can string together commands, just like in ruby, very interesting
-        pstats.Stats(pr).sort_stats('time').print_stats('testing_watson')
+            # this is where the information of iteration goes, the process
+            sys.stdout = open(current_out_proc, 'w') # don't need to worry about time exception create loss of data, since it print out as process goes
+            #sys.stdout = sys.__stdout__
 
-        info = '\n' + str(i)
-        status_report(function_name, status_out, info)
+            pr.enable()
+            e = function(f, g, G, x, epsilon)
+            print("the end of the result: ", f(e))
+            pr.disable()
+
+            put_prof(pr, current_out_prof)
+
+            info = '\n' + str(i) + ' dimension for ' + function_name
+            status_report(status_out, info)
+
+        # reset the alarm in the exception
+        except OverTime:
+
+            info = ('\n' + str(i) + ' dimension for '
+                         + function_name + ' is too slow for this problem. already used up its time '
+                         + str(MAX_TIME) + '\n')
+
+            status_report(status_out, info)
+            #signal.alarm(MAX_TIME)
+            put_prof(pr, current_out_prof)
+            signal.setitimer(signal.ITIMER_REAL, MAX_TIME)
+
 
     return 0
 
+
+
 def test_quasi_newtons(function, function_name, start = 2, to = 31, epsilon = 10 ** (-4)):
     for i in range(start, to):
-        x = np.zeros(i)
-
-        current_out_proc = str(i) + function_name + '.proc'
-
-        # this is where the information of iteration goes, the process
-        sys.stdout = open(current_out_proc, 'w')
-
+        # declare here to allow except to access
         pr = cProfile.Profile()
-        pr.enable()
-
-        e = function(f, g, x, epsilon)
-        print("the end of the result: ", f(e))
-
-        pr.disable()
-
+        current_out_proc = str(i) + function_name + '.proc'
         current_out_prof = str(i) + function_name + '.prof'
 
-        # this is where the information of the profile goes
-        sys.stdout = open(current_out_prof, 'w')
+        try:
+            x = np.zeros(i)
 
-        # all methods return self, so you can string together commands, just like in ruby, very interesting
-        pstats.Stats(pr).sort_stats('time').print_stats('testing_watson')
+            # this is where the information of iteration goes, the process
+            sys.stdout = open(current_out_proc, 'w') # don't need to worry about time exception create loss of data, since it print out as process goes
+            #sys.stdout = sys.__stdout__
 
-        sys.stdout = open('status_report', 'w')
-        print(function_name, 'is done, proceeding!')
+            pr.enable()
+            e = function(f, g, x, epsilon)
+            print("the end of the result: ", f(e))
+            pr.disable()
+
+            put_prof(pr, current_out_prof)
+
+            info = '\n' + str(i) + ' dimension for ' + function_name
+            status_report(status_out, info)
+
+        # reset the alarm in the exception
+        except OverTime:
+
+            info = ('\n' + str(i) + ' dimension for '
+                         + function_name + ' is too slow for this problem. already used up its time '
+                         + str(MAX_TIME) + '\n')
+
+            status_report(status_out, info)
+            #signal.alarm(MAX_TIME)
+            put_prof(pr, current_out_prof)
+            signal.setitimer(signal.ITIMER_REAL, MAX_TIME)
 
     return 0
 
@@ -103,15 +134,14 @@ to = 32
 
 # setting pretty relatively high epsilon for newtons
 
-#test_quasi_newtons(me.SR1, 'SR1', start, to, 0.000001)
+#test_quasi_newtons(me.SR1, 'SR1', start, to, 0.0000001)
 #test_quasi_newtons(me.DFP, 'DFP', start, to, 0.000001)
 #test_quasi_newtons(me.BFGS, 'BFGS', start, to, 0.000001)
 
 #test_newtons(me.naive_newton, 'naive_newton')
 signal.signal(signal.SIGALRM, handler)
-signal.alarm(1)
+signal.setitimer(signal.ITIMER_REAL, MAX_TIME)
 test_newtons(me.damped_newton, 'damped_newton', start, to)
 test_newtons(me.compound_newton, 'compound_newton', start, to)
 test_newtons(me.LM, 'LM', start, to)
-
 signal.alarm(0)
