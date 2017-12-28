cf663297dcf14d6883b1409a145578a5ffbbf9ea
326623 <yangqp5@mail2.sysu.edu.cn>
2017-12-28 09:41:45 +0800

    upload flaskr code

--- implementation/project/flaskr/flaskr/flaskr.py
+++ implementation/project/flaskr/flaskr/flaskr.py
@@ -58,8 +58,11 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) valus (?, ?)', [request.form['title'], request.form['text']])
+    db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+    db.commit()
+    #print(request.form)
     flash('New entry was successfully posted')
+    #print(url_for('show_entries'))
     return redirect(url_for('show_entries'))
 
 @app.route('/login', methods=['GET', 'POST'])
