715223c5e243468afedd5a638226718245dd77e5
326623 <yangqp5@mail2.sysu.edu.cn>
2018-01-17 18:17:52 +0800

    delete BST

--- leetcode/450/450.cpp
+++ leetcode/450/450.cpp
@@ -8,11 +8,51 @@
  * };
  */
 class Solution {
-  TreeNode *p; // parent
+  TreeNode *p;
 public:
+  TreeNode* find_min_right(TreeNode *root) {
+    if(root->left) {
+      p = root;
+      return find_min_right(root->left);
+    }
+    else {
+      return root;
+    }
+  }
 
   TreeNode* deleteNode(TreeNode* root, int key) {
-    p = root;
-    return find_delete(root, key);
+    if(root) {
+      if(root->val == key) {
+        if(!root->left && !root->right) {
+          return NULL;
+        }
+        else if(!root->left) {
+          return root->right;
+        }
+        else if(!root->right) {
+          return root->left;
+        }
+        else {
+          // find the min of right, and replace it with root
+          p = root->right;
+          TreeNode *min = find_min_right(root->right);
+          p->left = (min->right ? min->right : NULL);
+          min->right = root->right;
+          min->left = root->left;
+          return min;
+        }
+      }
+      else if(root->val < key) {
+        root->left = deleteNode(root->left, key);
+        return root;
+      }
+      else {
+        root->right = deleteNode(root->right, key);
+        return root;
+      }
+    }
+    else {
+      return root;
+    }
   }
 };
