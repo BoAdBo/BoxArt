93185b4868f3afeeb916605c7fbc10273bc7eecf
326623 <yangqp5@mail2.sysu.edu.cn>
2018-01-17 22:11:40 +0800

    450 not actually deleting node(no free)

--- leetcode/450/450.cpp
+++ leetcode/450/450.cpp
@@ -7,6 +7,18 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+#include <stdlib.h>
+#include <stdio.h>
+
+#define node(val) new TreeNode(val)
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
   TreeNode *p;
 public:
@@ -20,9 +32,17 @@ public:
     }
   }
 
-  TreeNode* deleteNode(TreeNode* root, int key) {
+  TreeNode *deleteNode(TreeNode *root, int key) {
     if(root) {
-      if(root->val == key) {
+      if(key > root->val) {
+        root->right = deleteNode(root->right, key);
+        return root;
+      }
+      else if(key < root->val) {
+        root->left = deleteNode(root->left, key);
+        return root;
+      }
+      else {
         if(!root->left && !root->right) {
           return NULL;
         }
@@ -33,26 +53,62 @@ public:
           return root->left;
         }
         else {
-          // find the min of right, and replace it with root
-          p = root->right;
           TreeNode *min = find_min_right(root->right);
-          p->left = (min->right ? min->right : NULL);
-          min->right = root->right;
-          min->left = root->left;
-          return min;
+          min->left = root->left;// change the entire left subtree to the min node, and skip the root node
+          return root->right;
         }
-      }
-      else if(root->val < key) {
-        root->left = deleteNode(root->left, key);
-        return root;
-      }
-      else {
-        root->right = deleteNode(root->right, key);
-        return root;
       }
     }
     else {
-      return root;
+      return NULL;
     }
   }
+
 };
+
+
+// TreeNode* deleteNode(TreeNode* root, int key) {
+//   if(root) {
+//     if(root->val == key) {
+//       if(!root->left && !root->right) {
+//         return NULL;
+//       }
+//       else if(!root->left) {
+//         return root->right;
+//       }
+//       else if(!root->right) {
+//         return root->left;
+//       }
+//       else {
+//         // find the min of right, and replace it with root
+//         TreeNode *min = find_min_right(root->right);
+//         root->val = min->val;
+//         root->right = deleteNode(root->right, min->val);
+//         return root;
+//       }
+//     }
+//     else if(root->val < key) {
+//       root->right = deleteNode(root->right, key);
+//       return root;
+//     }
+//     else {
+//       root->left = deleteNode(root->left, key);
+//       return root;
+//     }
+//   }
+//   else {
+//     return root;
+//   }
+// }
+
+
+int main() {
+  TreeNode *root = node(5);
+  root->left = node(3);
+  root->right = node(6);
+  root->left->left = node(2);
+  root->left->right = node(4);
+  root->right->right = node(7);
+  Solution test;
+  test.deleteNode(root, 3);
+}
