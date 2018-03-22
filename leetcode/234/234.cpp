720370695302898d9925e50b4c0b0c5dc6c23ee0
326623 <yangqp5@mail2.sysu.edu.cn>
2018-03-22 23:26:38 +0800

    merge

--- leetcode/234/234.cpp
+++ leetcode/234/234.cpp
@@ -17,38 +17,19 @@ struct ListNode {
 class Solution1 {
 public:
   static bool isPalindrome(ListNode* head) {
-    ListNode *iter = head;
-    int count = 0;
-    if(head == NULL) return true;
-
-    while(iter != NULL) {
-      iter = iter->next;
-      count++;
-    }
-
-    ListNode *half = head;
-    for(int i = 0; i < count / 2; ++ i) {
-      half = half->next;
-    }
-
-    while(half != NULL) {
-      if(head->val != half->val) return false;
-      head = head->next;
-      half = half->next;
-    }
-
-    return true;
+    
   }
 };
 
-#include <stdio.h>
 class Solution : public Solution1 {
 };
 
+#include <stdio.h>
+
 int main() {
   ListNode *head = new ListNode(1);
-  head->next = new ListNode(2);
-  head->next->next = new ListNode(1);
+  //head->next = new ListNode(2);
+  //head->next->next = new ListNode(1);
   if(Solution::isPalindrome(head)) {
     printf("it is");
   }
