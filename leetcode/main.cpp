2cd5e025f79a6f18aa5184382bd75f0a4d25683f
king-kong <yangqp5@outlook.com>
2018-05-23 21:16:14 +0800

    learning concurrency

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,23 +1,126 @@
-#include "215/215.cpp"
-int main() {
-  Solution testing;
-  //vector<int> test = {3,2,1,5,6,4};
-  vector<int> test = {2,1};
+// //#include "215/215.cpp"
+// #include <iostream>
+// #include <string>
+// #include <sstream>
+// #include <cassert>
+// #include <vector>
+// #include <map>
 
-  cout << testing.findKthLargest(test, 1) << endl;
-  for (int i = 0; i < test.size(); ++ i) {
-    cout << test[i] << " ";
+// using std::vector;
+// using std::string;
+// using std::map;
+// std::ostream & operator<<(std::ostream &os, const vector<string> &hs) {
+//   for (const auto & h : hs) {
+//     os << h << " ";
+//   }
+//   return os;
+// }
+
+
+// class Solution {
+// public:
+//   vector<string> subdomainVisits(vector<string>& cpdomains) {
+//     vector<string> res;
+
+//     for (const auto &cpdomain : cpdomains)
+//       countDomain(cpdomain);
+
+//     for (std::map<vector<string>, int>::const_iterator iter = countMap.begin(); iter != countMap.end(); ++ iter)
+//       res.emplace_back(std::to_string(iter->second) + " " + bind(iter->first, '.'));
+
+//     return res;
+//   }
+
+// private:
+//   map<vector<string>, int> countMap;
+//   void countDomain(const std::string &s) {
+//     // first split into number and domain
+//     vector<string> num_domain = split(s, ' ');
+//     assert(num_domain.size() == 2);
+
+//     string num = num_domain[0], domain = num_domain[1];
+//     int count = stoi(num);
+
+//     vector<string> subDomain = split(domain, '.');
+//     for ( size_t i = 0; i < subDomain.size(); ++ i) {
+//       vector<string> tmp(subDomain.begin() + i, subDomain.end());
+//       auto iter = countMap.find(tmp);
+//       if (iter == countMap.end()) {
+//         countMap[tmp] = count;
+//       }
+//       else {
+//         countMap[tmp] += count;
+//       }
+
+//     }
+//   }
+
+//   // similar to split, just glue strings with delim
+//   static std::string bind(const vector<string> & strs, char delim) {
+//     string res;
+//     for (const auto& str : strs)
+//       res += (str + delim);
+//     // erase the final dot
+//     res.pop_back();
+//     return res;
+//   }
+
+//   static std::vector<std::string> split(const std::string &s, char delim) {
+//     std::stringstream ss(s);
+//     std::vector<std::string> elems;
+//     std::string item;
+//     while (std::getline(ss, item, delim)) {
+//       elems.emplace_back(item);
+//     }
+
+//     return elems;
+//   }
+// };
+
+// int main() {
+//   Solution Testing;
+//   vector<string> str = {"900 google.com", "800 planlab.com"};
+//   std::cout << Testing.subdomainVisits(str) << "\n";
+
+// }
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <iostream>
+
+struct Counter {
+  int value;
+
+  Counter() : value(0)
+  { }
+
+  void increment() {
+    ++value;
   }
-  cout << endl;
-  //vector<string> test = { "aba", "abb", "abc" };
+};
 
-  //cout << testing.longestCommonPrefix(test) << endl;
-  // vector<vector<int>> res=testing.generate(20);
-  // //vector<vector<int>>& res=testing.generate(1);
-  // for ( int i = 0; i < res.size(); ++ i ) {
-  //   for ( int j = 0; j < res[i].size(); ++ j ) {
-  //     cout << res[i][j] << " ";
-  //   }
-  //   cout << endl;
-  // }
+void hello() {
+  std::cout << "Hello from thread: " << std::this_thread::get_id() << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+int main() {
+  Counter counter;
+  std::vector<std::thread> threads;
+
+  for ( size_t i = 0; i < 5; ++ i ) {
+    threads.push_back(std::thread([&counter]() {
+                                    for ( int i = 0; i < 100; ++ i ) {
+                                      counter.increment();
+                                    }
+                                  }));
+  }
+
+  for ( auto &thread : threads ) {
+    thread.join();
+  }
+
+  std::cout << counter.value << '\n';
+
+  return 0;
 }
