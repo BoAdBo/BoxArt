a97b251dd07975966c8f48a631aac6fa82693e62
newjoy <yangqp5@outlook.com>
2019-01-04 14:09:53 +0800

    done 307

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -59,16 +59,21 @@
 // }
 
 #include "include/predefined.hpp"
-#include "49/49.cpp"
-int main() {
-  Solution test;
-  std::vector<std::string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-  auto results = test.groupAnagrams(strs);
+#include "307/307.cpp"
+// ["NumArray","update","update","update","sumRange","update","sumRange","update"]
+// [[[0,9,5,7,3]],[4,5],[1,7],[0,8],[1,2],[1,9],[4,4],[3,4]]
 
-  for (const auto & strs : results) {
-    for (const auto & str : strs) {
-      std::cout << str << ' ';
-    }
-    std::cout << '\n';
-  }
+int main() {
+  std::vector<int> a = {0, 9, 5, 7, 3};
+  NumArray* ptrArray = new NumArray(a);
+  NumArray& array = *ptrArray;
+  std::cout << array.sumRange(4, 4) << '\n';
+  std::cout << array.sumRange(2, 4) << '\n';
+  std::cout << array.sumRange(3, 3) << '\n';
+  array.update(4, 5);
+  array.update(1, 7);
+  array.update(0, 8);
+  array.update(1, 9);
+  array.update(3, 4);
+  std::cout << array.sumRange(4, 4) << '\n';
 }
