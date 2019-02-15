48814d4b1f8e905ae2d6689b391d7520635d4dfe
king-kong <yangqp5@outlook.com>
2019-02-15 20:59:59 +0800

    689

--- leetcode/44/44.cpp
+++ leetcode/44/44.cpp
@@ -3,12 +3,62 @@
 class Solution {
 public:
   bool isMatch(string s, string p) {
-    // match the substring that is not asterisk first, then match asterisk
-    for( int i = 0; i < p.size(); ++ i ) {
-      if( p[i] == '*') {
-        
-      }
+    return match(s.cbegin(), s.cend(), p.cbegin(), p.cend());
+  }
 
+  // passed on 2019.2.15
+  template <typename Iterator>
+  bool match(Iterator first1, Iterator last1,
+             Iterator first2, Iterator last2) {
+    if (first2 == last2) return first1 == last1;
+    Iterator matched;
+    Iterator star = last2;
+
+    while (first1 != last1) {
+      if (first2 != last2) {
+        if (*first1 == *first2 || *first2 == '?') { ++first1; ++first2; continue; }
+        else if (*first2 == '*') { star = first2; matched = first1; ++first2; continue; }
+      }
+      // cannot continue with the match, fall back to the last '*', skip one character
+      if (star != last2) { first2 = star+1; first1 = matched+1; ++matched; continue; }
+      return false;
     }
+
+    // consume all '*' in the back
+    while (first2 != last2 && *first2 == '*') ++first2;
+    return first2 == last2;
+  }
+
+  // slow recursive version, TLE
+  template <typename Iterator>
+  bool match(Iterator first1, Iterator last1,
+             Iterator first2, Iterator last2) {
+    if (first1 == last1 && first2 == last2)
+      return true;
+
+    // first1 == last1
+    if (first1 == last1) {
+      // first1 == last1 && first2 != last2
+      if (*first2 == '*') {
+        return match(first1, last1, first2+1, last2);
+      }
+      else return false;
+    }
+
+    // first1 != last1 && first2 != last2
+    // && *first2 == '*'
+    if (first2 != last2) {
+      if (*first2 == '*') {
+        return match(first1+1, last1, first2, last2) ||
+          match(first1, last1, first2+1, last2);
+      }
+      else {
+        if (*first1 == *first2 || *first2 == '?')
+          return match(first1+1, last1, first2+1, last2);
+      }
+    }
+
+    // first1 != last1 && first2 == last2
+    return false;
   }
 };
---
+++ leetcode/689/689.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+  vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+    // two vector to store the left and right best candidates
+    // one vector to store the W, where W[i] is the sum of nums[i:i+k]
+
+  }
+};
